1bdff16f9a028140b5ed9f5c5813f2b67d2eb4ce
Abdalslam Aljmal <abodajaljmal@gmail.com>
2020-11-30 20:15:35 +0200

    fixed http

--- lib/data/webservice/EEWebService.dart
+++ lib/data/webservice/EEWebService.dart
@@ -1,11 +1,53 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:http/io_client.dart';
 import 'package:E_Emergency/domain/Interface/EEWebServiceInterface.dart';
 import 'package:http/http.dart' as http;
+
+
+
 
 class EEWebService implements EEWebServiceInterface {
 static const String URL="https://192.168.1.31:44390/ReciveHelpRequest";
 
+Future<bool> sendHelpRequest1(String phoneNumber,String latitude,String longitude) async {
+     try{
+           
+
+     var map = { 
+         'civilianPhoneNumber': phoneNumber,
+         'latitude': latitude,
+         'longitude': longitude,
+         'status':"Mcok",
+         'description':'carCrash',
+      };
+      var head={
+        'Content-Type': 'application/json; charset=UTF-8'
+      };
+      
+      dynamic headers = {'Accept': 'application/json'};
+      var response =await http.post(Uri.encodeFull(URL) ,body:map  ,);
+      
+  
+     
+        if(response.statusCode==200||response.statusCode==201||response.statusCode==202) {
+       
+           print("sent correctly");
+          return true;
+         
+        } else {
+           print("not sent"+response.toString());
+          return false;
+        }     
+     }catch(e){
+       print(e);
+
+     }
+ 
+
+  
+
+   }
 /*
 *this method takes the civilian phone number, latitude and longitude as a string 
 *and sends it via http post to the api
@@ -17,17 +59,10 @@ static const String URL="https://192.168.1.31:44390/ReciveHelpRequest";
    Future<bool> sendHelpRequest(String phoneNumber,String latitude,String longitude) async {
      try{
        HttpClient client = new HttpClient();
-        client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);       
-Map<String, dynamic> toJson(){
-  return{
-     'civilianPhoneNumber': phoneNumber,
-         'latitude': latitude,
-         'longitude': longitude,
-         'status':"Mcok",
-         'description':'carCrash',
-  };
-}
-     Map<String,String> map = { 
+        client.badCertificateCallback = ((X509Certificate cert, String host, int port) => true);
+               
+
+     Map<dynamic,dynamic> map = { 
          'civilianPhoneNumber': phoneNumber,
          'latitude': latitude,
          'longitude': longitude,
@@ -35,19 +70,18 @@ Map<String, dynamic> toJson(){
          'description':'carCrash',
       };
       
-
-    HttpClientRequest request = await client.postUrl(Uri.parse(URL));
-
-    request.headers.set('Content-Type', 'application/json; charset=UTF-8');
-   
+ 
+   var head={
+        'Content-Type': 'application/json; charset=UTF-8'
+      };
+    var ioClient = new IOClient(client);
     
-    request.add(utf8.encode(json.encode(toJson())));
+     http.Response response = await ioClient.post(Uri.parse(URL), body: map,);
+  
 
-    HttpClientResponse response = await request.close();
-
-    String reply = await response.transform(utf8.decoder).join();
+   String reply = await response.body.toString();
     print("sent ");
-    print(reply);     
+    print(response.body);     
      
         if(response.statusCode==200||response.statusCode==201||response.statusCode==202) {
           //if(httpResponse.body=="true")
@@ -55,12 +89,12 @@ Map<String, dynamic> toJson(){
           return true;
          
         } else {
-           print("not sent"+response.headers.toString());
+           print("not sent"+response.statusCode.toString());
           return false;
         }     
      }catch(e){
        print(e);
-
+       return false;
      }
  
 
